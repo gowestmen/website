4e0b98eaafb9fb2e37f101530356a3da4620087c
gowestmen <126822329+gowestmen@users.noreply.github.com>
2024-10-21 22:37:25 +0200

    adding Photoswipe

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// Exemple de script JavaScript simple
-console.log("The website is currently under construction.");
+import PhotoSwipeLightbox from 'node_modules/photoswipe/dist/photoswipe-lightbox.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#gallery--getting-started',
+  children: 'a',
+
+  // setup PhotoSwipe Core dynamic import
+  pswpModule: () => import('node_modules/photoswipe/dist/photoswipe-lightbox.esm.js')
+});
+lightbox.init();
